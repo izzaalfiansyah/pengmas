dea6985cf79a404e12735db6bdddbd4e80013a1c
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-08-10 18:55:22 +0700

    change dashboard & login with database from gigih

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -6,6 +6,172 @@ import { Chart, registerables } from "chart.js";
 import { useLoading } from "@/contexts/loading-context";
 import http from "@/libs/http";
 
+// export default function () {
+//   let canvas = useRef<any>();
+
+//   const [chart, setChart] = useState<Chart>();
+//   const [kondisi, setKondisi] = useState<any>();
+//   const [perangkat_id, setPerangkat_id] = useState("0");
+//   const [isError, setIsError] = useState(false);
+
+//   const loading = useLoading();
+
+//   const render = async () => {
+//     Chart.register(...registerables);
+
+//     const ctx = canvas.current.getContext("2d");
+//     const ch = new Chart(ctx, {
+//       type: "line",
+//       data: {
+//         labels: [],
+//         datasets: [
+//           {
+//             label: "cm",
+//             data: [],
+//             fill: true,
+//             borderColor: "#fbbc04",
+//             backgroundColor: "rgba(251,188,4,0.05)",
+//             tension: 0.1,
+//           },
+//         ],
+//       },
+//       options: {
+//         maintainAspectRatio: false,
+//         hover: {
+//           intersect: true,
+//         },
+//         plugins: {
+//           filler: {
+//             propagate: false,
+//           },
+//         },
+//         scales: {
+//           y: {
+//             beginAtZero: true,
+//             max: 100,
+//             min: 0,
+//             ticks: {
+//               stepSize: 10,
+//             },
+//           },
+//         },
+//       },
+//     });
+
+//     setChart(ch);
+//   };
+
+//   const getData = async () => {
+//     await http()
+//       .get("/air/latest", {
+//         params: {
+//           perangkat_id,
+//         },
+//       })
+//       .then((res) => {
+//         setIsError(false);
+//         setKondisi(res.data);
+
+//         chart?.data.labels?.push(res.data.waktu);
+//         chart?.data.datasets[0].data?.push(res.data.ketinggian);
+
+//         (chart as any).data.labels = chart?.data.labels?.slice(-9);
+//         (chart as any).data.datasets[0].data =
+//           chart?.data.datasets[0].data.slice(-9);
+
+//         chart?.update();
+//       })
+//       .catch((e) => {
+//         setIsError(true);
+//       });
+//   };
+
+//   const destroyChart = () => {
+//     (chart as any).data.labels = [];
+//     (chart as any).data.datasets[0].data = [];
+//     chart?.update();
+//   };
+
+//   useEffect(() => {
+//     render();
+//   }, []);
+
+//   useEffect(() => {
+//     if (chart) {
+//       loading?.show(getData);
+//     }
+//   }, [chart, perangkat_id]);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       getData();
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+//   });
+
+//   return (
+//     <AdminLayout title="Dashboard">
+//       {isError && (
+//         <div className="mb-4">
+//           <div className="bg-orange-100 text-orange-400 border border-orange-300 rounded text-sm p-4">
+//             Terjadi kesalahan, Gagal mengambil data kondisi air.
+//           </div>
+//         </div>
+//       )}
+//       <div className="flex lg:flex-row flex-col gap-x-5">
+//         <div className="lg:w-2/5">
+//           <Card title="Pilihan Perangkat">
+//             <select
+//               className="rounded border border-gray-300 h-10 bg-white px-3 w-full outline-none focus:shadow-sm focus:ring-2 focus:ring-orange-200 focus:border-orange-400 transition disabled:bg-gray-50"
+//               value={perangkat_id}
+//               onChange={(e) => {
+//                 setPerangkat_id(e.currentTarget.value);
+//                 destroyChart();
+//               }}
+//             >
+//               <option value="0">Perangkat 0</option>
+//               <option value="1">Perangkat 1</option>
+//             </select>
+//           </Card>
+//         </div>
+//         <div className="flex-1">
+//           <Card>
+//             <div className="flex items-center justify-between">
+//               <div className="flex-1">
+//                 <div className="mb-5 text-gray-800 text-lg">Ketinggian Air</div>
+//                 <div className="text-3xl mb-3 text-primary">
+//                   {kondisi?.ketinggian}
+//                 </div>
+//                 <div>
+//                   status :{" "}
+//                   <span
+//                     className={
+//                       ["text-green-500", "text-yellow-500", "text-red-500"][
+//                         parseInt(kondisi?.status) - 1
+//                       ]
+//                     }
+//                   >
+//                     {kondisi?.status_detail}
+//                   </span>
+//                 </div>
+//               </div>
+//               <div className="rounded-full bg-primary bg-opacity-25 p-3">
+//                 <ChartBarIcon className="text-primary w-8 h-8" />
+//               </div>
+//             </div>
+//           </Card>
+//         </div>
+//       </div>
+//       <Card title="Grafik Ketinggian Air">
+//         <div className="chart chart-sm">
+//           <canvas ref={canvas} className="h-[400px]"></canvas>
+//         </div>
+//       </Card>
+//     </AdminLayout>
+//   );
+// }
+
 export default function () {
   let canvas = useRef<any>();
 
@@ -48,7 +214,7 @@ export default function () {
         scales: {
           y: {
             beginAtZero: true,
-            max: 100,
+            max: 300,
             min: 0,
             ticks: {
               stepSize: 10,
@@ -119,50 +285,31 @@ export default function () {
           </div>
         </div>
       )}
-      <div className="flex lg:flex-row flex-col gap-x-5">
-        <div className="lg:w-2/5">
-          <Card title="Pilihan Perangkat">
-            <select
-              className="rounded border border-gray-300 h-10 bg-white px-3 w-full outline-none focus:shadow-sm focus:ring-2 focus:ring-orange-200 focus:border-orange-400 transition disabled:bg-gray-50"
-              value={perangkat_id}
-              onChange={(e) => {
-                setPerangkat_id(e.currentTarget.value);
-                destroyChart();
-              }}
-            >
-              <option value="0">Perangkat 0</option>
-              <option value="1">Perangkat 1</option>
-            </select>
-          </Card>
-        </div>
-        <div className="flex-1">
-          <Card>
-            <div className="flex items-center justify-between">
-              <div className="flex-1">
-                <div className="mb-5 text-gray-800 text-lg">Ketinggian Air</div>
-                <div className="text-3xl mb-3 text-primary">
-                  {kondisi?.ketinggian}
-                </div>
-                <div>
-                  status :{" "}
-                  <span
-                    className={
-                      ["text-green-500", "text-yellow-500", "text-red-500"][
-                        parseInt(kondisi?.status) - 1
-                      ]
-                    }
-                  >
-                    {kondisi?.status_detail}
-                  </span>
-                </div>
-              </div>
-              <div className="rounded-full bg-primary bg-opacity-25 p-3">
-                <ChartBarIcon className="text-primary w-8 h-8" />
-              </div>
+      <Card>
+        <div className="flex items-center justify-between">
+          <div className="flex-1">
+            <div className="mb-5 text-gray-800 text-lg">Ketinggian Air</div>
+            <div className="text-3xl mb-3 text-primary">
+              {kondisi?.ketinggian}
             </div>
-          </Card>
+            <div>
+              status :{" "}
+              <span
+                className={
+                  ["text-green-500", "text-yellow-500", "text-red-500"][
+                    parseInt(kondisi?.status) - 1
+                  ]
+                }
+              >
+                {kondisi?.status_detail}
+              </span>
+            </div>
+          </div>
+          <div className="rounded-full bg-primary bg-opacity-25 p-5">
+            <ChartBarIcon className="text-primary w-16 h-16" />
+          </div>
         </div>
-      </div>
+      </Card>
       <Card title="Grafik Ketinggian Air">
         <div className="chart chart-sm">
           <canvas ref={canvas} className="h-[400px]"></canvas>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,8 +5,98 @@ import { useRouter } from "next/router";
 import loginSVG from "@/images/login.svg";
 import Image from "next/image";
 
+// export default function () {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState<any>({});
+
+//   const loading = useLoading();
+//   const router = useRouter();
+
+//   const login = async () => {
+//     try {
+//       const res = await http().post("/login", { email, password });
+//       localStorage.setItem("_token", res.data.data);
+//       router.replace("/admin");
+//     } catch (err: any) {
+//       setError(err);
+//     }
+//   };
+
+//   const handleSubmit = async (e: any) => {
+//     e.preventDefault();
+//     loading?.show(login);
+//   };
+
+//   return (
+//     <div className="min-h-screen bg-white flex text-gray-700">
+//       <div className="grow flex-1 lg:flex justify-center items-center hidden border-r bg-gray-50 bg-opacity-50">
+//         <Image
+//           src={loginSVG}
+//           alt="Login SVG"
+//           width={1200}
+//           height={1200}
+//           className="max-h-screen max-w-screen p-10 px-20"
+//         />
+//       </div>
+//       <div className="lg:w-[470px] w-full flex min-h-screen items-center p-10">
+//         <form onSubmit={handleSubmit} className="w-full">
+//           <div className="text-lg font-semibold">Selamat Datang! 👋🏻</div>
+//           <p className="text-sm">
+//             Masuk ke akun anda untuk memulai sesi aplikasi
+//           </p>
+//           <div className="pt-10"></div>
+//           <div className="mb-5">
+//             <div className="mb-2 mt-1 text-sm">Email</div>
+//             <input
+//               type="text"
+//               className="rounded border border-gray-300 h-10 bg-white px-3 w-full outline-none focus:shadow-sm focus:ring-2 focus:ring-orange-200 focus:border-orange-400 transition disabled:bg-gray-50"
+//               placeholder="Masukkan Email"
+//               value={email}
+//               onChange={(e) => setEmail(e.currentTarget.value)}
+//               disabled={loading?.value}
+//             />
+//             <div className="text-xs text-red-500">{error.email}</div>
+//           </div>
+//           <div className="mb-5">
+//             <div className="mb-2 mt-1 text-sm">Password</div>
+//             <input
+//               type="password"
+//               className="rounded border border-gray-300 h-10 bg-white px-3 w-full outline-none focus:shadow-sm focus:ring-2 focus:ring-orange-200 focus:border-orange-400 transition disabled:bg-gray-50"
+//               placeholder="********"
+//               name="password"
+//               value={password}
+//               onChange={(e) => setPassword(e.currentTarget.value)}
+//               disabled={loading?.value}
+//             />
+//             <div className="text-xs text-red-500">{error.password}</div>
+//           </div>
+//           <div className="flex-inline">
+//             <input
+//               type="checkbox"
+//               id="remember"
+//               className="rounded text-primary mr-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-200"
+//             />
+//             <label htmlFor="remember" className="text-sm">
+//               Ingat Saya
+//             </label>
+//           </div>
+//           <div className="pt-10"></div>
+//           <button
+//             type="submit"
+//             className="bg-primary text-white w-full text-center shadow-sm rounded h-10 outline-none disabled:bg-opacity-75 transition"
+//             disabled={loading?.value}
+//           >
+//             {loading?.value ? "Memuat..." : "Masuk"}
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
 export default function () {
-  const [email, setEmail] = useState("");
+  const [nama_lengkap, setNama_lengkap] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<any>({});
 
@@ -15,7 +105,7 @@ export default function () {
 
   const login = async () => {
     try {
-      const res = await http().post("/login", { email, password });
+      const res = await http().post("/login", { nama_lengkap, password });
       localStorage.setItem("_token", res.data.data);
       router.replace("/admin");
     } catch (err: any) {
@@ -47,16 +137,16 @@ export default function () {
           </p>
           <div className="pt-10"></div>
           <div className="mb-5">
-            <div className="mb-2 mt-1 text-sm">Email</div>
+            <div className="mb-2 mt-1 text-sm">Nama Lengkap</div>
             <input
               type="text"
               className="rounded border border-gray-300 h-10 bg-white px-3 w-full outline-none focus:shadow-sm focus:ring-2 focus:ring-orange-200 focus:border-orange-400 transition disabled:bg-gray-50"
-              placeholder="Masukkan Email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
+              placeholder="Masukkan Nama Lengkap"
+              value={nama_lengkap}
+              onChange={(e) => setNama_lengkap(e.currentTarget.value)}
               disabled={loading?.value}
             />
-            <div className="text-xs text-red-500">{error.email}</div>
+            <div className="text-xs text-red-500">{error.nama_lengkap}</div>
           </div>
           <div className="mb-5">
             <div className="mb-2 mt-1 text-sm">Password</div>
